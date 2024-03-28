9979e8f6a7245e39e3732679aa4efc4827fa898d
Alexander Linse <zandervon24@gmail.com>
2024-03-28 15:50:34 -0500

    Styling revolving around spacing

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -2,10 +2,10 @@ function Pizza ({pizza, removeSelf}) {
   const displayKeys = Object.keys(pizza);
   displayKeys.splice(displayKeys.indexOf("name"), 1);
 
-  return  <section className="containerItem">
+  return <section className="containerItem containerPizza">
     <h2>{pizza.name}</h2>
-    { displayKeys.map((k, i) => <div key={i}>{k}: {pizza[k].toString()}</div>) }
-    <button onClick={removeSelf} style={{color:"red", alignSelf:"flex-end"}}>X</button>
+    <div>{ displayKeys.map((k, i) => <div key={i}>{k}: {pizza[k].toString()}</div>) }</div>
+    <button onClick={removeSelf} style={{color:"red"}}>X</button>
   </section>
 }
 
