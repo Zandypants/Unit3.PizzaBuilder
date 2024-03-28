e5c7f8cfd4f37e6bd3058dfd438903c4952005ea
Alexander Linse <zandervon24@gmail.com>
2024-03-28 16:40:00 -0500

    Refactored form submission, trimmed state

--- src/PizzaForm.jsx
+++ src/PizzaForm.jsx
@@ -1,31 +1,28 @@
-import { useState } from "react";
 import { samplePizza } from "./App"
 
-function PizzaForm ({addPizza}) {
-  const [newPizza, setNewPizza] = useState(samplePizza);
-
-  const onInputChange = (event) => {
-    const editPizza = {...newPizza};
-    editPizza[event.target.name] = event.target.value;
-    setNewPizza(editPizza);
-  }
-
+function PizzaForm({ addPizza }) {
   const onSubmit = (event) => {
     event.preventDefault();
-    
-    addPizza(newPizza);
-
     const inputs = event.target.querySelectorAll("input");
-    inputs.forEach(input => input.value = "");
+
+    const newPizza = { ...samplePizza };
+    inputs.forEach(input => {
+      if (input.type != "text" || input.value === "") return;
+
+      newPizza[input.name] = input.value;
+      input.value = "";
+    });
+    addPizza(newPizza);
   }
 
   return <form className="containerItem" onSubmit={onSubmit}>
-    <h2 style={{color:"green"}}>Custom</h2>
-      <p>(separate multiple inputs with a comma)</p>
-    { Object.keys(samplePizza).map((k, i) => {
+    <h2 style={{ color: "green" }}>Custom</h2>
+    <p>(separate multiple inputs with a comma)</p>
+    {
+      Object.keys(samplePizza).map((k, i) => {
         return <div key={i} className="inputLine">
           <label htmlFor={k}>{k}: </label>
-          <input type="text" name={k} onChange={onInputChange}></input>
+          <input type="text" name={k}></input>
         </div>
       })
     }
